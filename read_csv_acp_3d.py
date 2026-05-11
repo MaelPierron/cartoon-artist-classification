a665e943d5b55027d075881af7b47f880d9458eb
MessengerYan <jmaxag@gmail.com>
2026-05-11 08:50:17 +0200

    Amélioration ACP et ACP 3d

--- read_csv_acp_3d.py
+++ read_csv_acp_3d.py
@@ -18,26 +18,29 @@ selector = SelectKBest(score_func=f_classif, k=400)
 X_reduced = selector.fit_transform(X, y)
 
 # 🔥 3. PCA
-pca = PCA(n_components=2)
-X_2d = pca.fit_transform(X_reduced)
+pca = PCA(n_components=3)
+X_3d = pca.fit_transform(X_reduced)
 
 # Labels numériques
 labels = pd.factorize(y)[0]
 
-# 🔥 Affichage
-plt.figure(figsize=(8,6))
+# 🔥 4. Plot 3D
+fig = plt.figure(figsize=(10,7))
+ax = fig.add_subplot(111, projection='3d')
 
-scatter = plt.scatter(
-    X_2d[:,0],
-    X_2d[:,1],
+scatter = ax.scatter(
+    X_3d[:,0],
+    X_3d[:,1],
+    X_3d[:,2],
     c=labels,
     cmap='tab10',
     alpha=0.7
 )
 
-plt.title("Projection PCA des meilleures features")
-plt.xlabel("PC1")
-plt.ylabel("PC2")
+ax.set_title("Projection PCA 3D des features")
+ax.set_xlabel("PC1")
+ax.set_ylabel("PC2")
+ax.set_zlabel("PC3")
 
 plt.colorbar(scatter)
 plt.show()
