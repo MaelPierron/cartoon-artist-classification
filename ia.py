8353ffc3651e4323ec70d6683f6c9a79c70689bd
Maël PIERRON <mael.pierron@cpe.fr>
2026-05-11 09:17:49 +0200

    commentaire ia

--- ia.py
+++ ia.py
@@ -4,18 +4,15 @@ import torch.optim as optim
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
 
-# Device (GPU si dispo)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Transformations (IMPORTANT)
 transform = transforms.Compose([
     transforms.Resize((384, 384)),
-    transforms.ToTensor(),
+    transforms.ToTensor(), # transformation de l'image en matrice exploitable par le système
     transforms.Normalize([0.5, 0.5, 0.5],
-                         [0.5, 0.5, 0.5])
+                         [0.5, 0.5, 0.5]) # on normalise entre -1 et 1 pour faciliter l'apprentissage
 ])
 
-# Chargement dataset
 train_dataset = datasets.ImageFolder("dataset/train", transform=transform)
 val_dataset = datasets.ImageFolder("dataset/val", transform=transform)
 
@@ -25,18 +22,14 @@ val_loader = DataLoader(val_dataset, batch_size=16)
 num_classes = len(train_dataset.classes)
 print("Classes:", train_dataset.classes)
 
-# 🔥 Modèle pré-entraîné (très important)
-model = models.resnet18(pretrained=True)
+model = models.resnet18(pretrained=True) # on charge le modèle préentraîné resnet18
 
-# On remplace la dernière couche
-model.fc = nn.Linear(model.fc.in_features, num_classes)
+model.fc = nn.Linear(model.fc.in_features, num_classes) #on adapte le modèle à notre nombre de classes
 model = model.to(device)
 
-# Loss + optimiseur
-criterion = nn.CrossEntropyLoss()
+criterion = nn.CrossEntropyLoss() # choix, de la loss, EntropyLoss classique pour classification d'images
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-# 🔁 Entraînement
 epochs = 10
 
 for epoch in range(epochs):
@@ -56,21 +49,19 @@ for epoch in range(epochs):
 
     print(f"Epoch {epoch+1}, Loss: {total_loss:.4f}")
 
-    # Validation
     model.eval()
     correct = 0
     total = 0
 
-    with torch.no_grad():
+    with torch.no_grad(): # on desactive le calcul des gradients pour la validation pour aller plus vite
         for images, labels in val_loader:
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
 
-            _, predicted = torch.max(outputs, 1)
+            _, predicted = torch.max(outputs, 1) # on récupère la classe qui a un score maximal
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
     print(f"Validation Accuracy: {100 * correct / total:.2f}%")
 
-# 💾 Sauvegarde
 torch.save(model.state_dict(), "model.pth")
